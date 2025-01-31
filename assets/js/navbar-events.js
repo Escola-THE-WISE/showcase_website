7fb1ae0477001b4c2b4ab55dbd42fa99ec4aa784
Aluno Gustavo Costa <123gustaf321@gmail.com>
2025-01-31 15:42:49 -0300

    add content | scroll navigation

--- assets/js/navbar-events.js
+++ assets/js/navbar-events.js
@@ -1,3 +1,4 @@
+/* Abre e fecha menu mobile */
 var hamburguerMenus = document.querySelectorAll(".m--nav__hamburguer-menu");
 var headerNavigations = document.querySelectorAll(".l--header-navigation");
 
@@ -11,4 +12,17 @@ hamburguerMenus.forEach(hamburguerMenu => {
     });
 
     
+});
+
+/* ------------------------------------------------------------------------------------- */
+/* Muda o Header ao Scrollar */
+var navigationScrool = document.querySelector(".l--header-navigation");
+
+window.addEventListener("scroll", function(){
+    if(window.scrollY > 1){
+        navigationScrool.classList.add("is-scrolled");
+    }
+    else{
+        navigationScrool.classList.remove("is-scrolled");
+    }
 });
