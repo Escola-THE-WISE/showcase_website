e05c1f5a7223a16df4c50989e1224f550e93dd1a
Aluno Gustavo Costa <gustavosncosta@gmail.com>
2025-02-23 21:25:05 -0300

    update navigation

--- assets/js/navbar-events.js
+++ assets/js/navbar-events.js
@@ -1,13 +1,21 @@
 /* Abre e fecha menu mobile */
 var hamburguerMenus = document.querySelectorAll(".m--nav__hamburguer-menu");
 var headerNavigations = document.querySelectorAll(".l--header-navigation");
+var navLinks = document.querySelectorAll(".m--nav__link");
 
 hamburguerMenus.forEach(hamburguerMenu => {
     hamburguerMenu.addEventListener("click", () => {
-        hamburguerMenu.classList.toggle("is-active")
+        hamburguerMenu.classList.toggle("is-active");
 
         headerNavigations.forEach(headerNavigation => {
-            headerNavigation.classList.toggle("is-active")
+            headerNavigation.classList.toggle("is-active");
+
+            navLinks.forEach(navLink =>{
+                navLink.addEventListener("click", ()=> {
+                    headerNavigation.classList.remove("is-active");
+                    hamburguerMenu.classList.remove("is-active");
+                })
+            })
         });
     });
 
