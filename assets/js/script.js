98e78bea192e6be804138c6e80c4903a90a6613f
Aluno Gustavo Costa <gustavosncosta@gmail.com>
2025-02-26 23:50:36 -0300

    js carrossel

--- assets/js/script.js
+++ assets/js/script.js
@@ -9,7 +9,7 @@ document.addEventListener( 'DOMContentLoaded', function() {
         pagination: boolean = true,
         paginationKeyboard: boolean = true,
         classes: {
-            pagination: 'splide__pagination m--carousel__pagination-container m--carousel-timeline__pagination-container',
+            pagination: 'm--carousel__pagination-container m--carousel-timeline__pagination-container',
             page      : 'splide__pagination__page m--carousel__pagination m--carousel-timeline__pagination',
         }
       } );
@@ -22,16 +22,34 @@ document.addEventListener( 'DOMContentLoaded', function() {
     carousels.forEach(carousel =>{
         var carouselMount = new Splide( carousel, {
             type: 'loop',
-            arrows: boolean = false,
+            arrows: boolean = true,
             paginationKeyboard: boolean = true,
             classes: {
                 pagination: 'splide__pagination m--carousel__pagination-container',
                 page      : 'splide__pagination__page m--carousel__pagination',
+                arrows: 'splide__arrows m--carousel__arrow-container',
+		        arrow : 'splide__arrow m--carousel__arrow',
+		        prev  : 'splide__arrow--prev m--carousel__arrow-prev',
+		        next  : 'splide__arrow--next m--carousel__arrow-next',
             }
         });
         carouselMount.mount();
     })
       
-    
+    document.querySelectorAll('.m--carousel-timeline__pagination').forEach(pagination =>{
+        pagination.addEventListener('click', function(){
+
+            document.querySelectorAll(' ').forEach(itemLista =>{
+
+                if(pagination.classList.contains('is-active')){
+                    itemLista.classList.add('is-active');
+                }
+                else{
+                    itemLista.classList.remove('is-active');
+                }
+            })
+        });
+
+    });
 } );
 
